791294536adef677e2c2d021d46adfc0897791bb
Prakhar Srivastav <prakhar1989@gmail.com>
2012-06-22 11:21:47 +0530

    Completed Admin Interface

--- main.py
+++ main.py
@@ -30,6 +30,54 @@ class Coupon(db.Model):
         self.is_valid = False
 
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    candidates = db.relationship('Candidate', backref='post', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Post %r>" % self.name
+
+
+class Candidate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    hostel = db.Column(db.String(80))
+    yes_no = db.Column(db.Boolean)
+    #votes = db.relationship('Vote', backref='vote', lazy="dynamic")
+
+    def __init__(self, name, hostel, post_id, yes_no):
+        self.name = name
+        self.hostel = hostel
+        self.post_id = post_id
+        self.yes_no = yes_no
+
+    def __repr__(self):
+        return "<Candidate: %r, Post: %r>" % (self.name, self.post.name)
+
+#class Vote(db.Model):
+    ##under substantial construction
+    ##Add users table, and make correct relationships
+    #id = db.Column(db.Integer, primary_key=True)
+    #user_id = db.Column(db.String(80))
+    #candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'))
+    #weightage = db.Column(db.Integer)
+    #post_name = db.Column(db.String(80)) #change this to a relationship
+
+    #def __init__(self, user_id, candidate_id, post_name, weightage=1):
+        #self.user_id = user_id
+        #self.candidate_id = candidate_id
+        #self.weightage = weightage
+        #self.post_name = post_name
+
+    #def __repr__(self):
+        #return "<Vote | user_id: %r -> candidate_id: %r for %r" % (self.user_id,
+                                    #self.candidate_id, self.post_name)
+
 ### CONTROLLER ###
 @app.route('/', methods=["GET", "POST"])
 def voting_page():
@@ -102,7 +150,8 @@ def admin():
             flash("You need to log in as admin to view the admin page", "error")
             return redirect(url_for('login'))
         else:
-            return render_template("admin_interface.html")
+            posts = Post.query.all()
+            return render_template("admin_interface.html", posts = posts)
 
 
 @app.route('/coupon/new', methods=['POST'])
@@ -118,6 +167,37 @@ def generate_coupon():
     else:
         return jsonify(coupon=value, msg="Incorrect Admin password")
 
+
+@app.route('/candidate/new', methods=['POST'])
+def save_candidate():
+    username = request.form['username']
+    post_select = request.form['post_select']
+    if "binary_vote" in request.form:
+        add_yesno = True
+    else:
+        add_yesno = False
+    hostel = ldap_helper.ldap_fetch_detail(username, ["hostel"])["hostel"]
+    if username:
+        candidate_post = Post.query.filter_by(name=post_select).first()
+        c1 = Candidate(username, hostel, candidate_post.id, add_yesno)
+        db.session.add(c1)
+        db.session.commit()
+        return jsonify(status_msg="Added %s to the database" % username)
+    else:
+        return jsonify(status_msg="To add a blank user, enter blank in username field")
+
+
+@app.route('/candidate/<username>')
+def fetch_candidate_details(username):
+    attr_list = ["departmentNumber", "displayName", "hostel"]
+    candidate_details = ldap_helper.ldap_fetch_detail(username, attr_list)
+    img_url = "http://student.iimcal.ac.in/userimages/%s.jpg" % username
+    if candidate_details:
+        return jsonify(image_url=img_url, dept=candidate_details["departmentNumber"],
+                                    name=candidate_details["displayName"],
+                                    hostel=candidate_details["hostel"])
+    else:
+        return jsonify(name="No candidate found!")
 
 if __name__ == "__main__":
     app.run()
--- static/js/application.js
+++ static/js/application.js
@@ -1,6 +1,7 @@
 $(function() {
     //hide the show_message label by default
-    $('#show_message').hide();
+    $('.coupon_gen').hide();
+    //$('#candidate_details').hide();
 
     $('#coupon_form').submit(function(){
         $.ajax({
@@ -10,9 +11,42 @@ $(function() {
             success: function(data) {
                 $('#coupon_value').text(data.coupon);
                 $('#show_message').text(data.msg);
-                $('#show_message').show();
+                $('.coupon_gen').show();
             }
         });
+        return false;
+    });
+
+    
+    $('#user_name').focusout(function(){
+        var username = $("#user_name").val();
+        var details_url = $SCRIPT_ROOT + '/candidate/' + username;
+        var details_template = "\
+            <img src={{image_url}} width='100px' height=auto> \
+            <dl class='dl-horizontal'> \
+                <dt>Name</dt> <dd>{{ name }}</dd> \
+                <dt>Hostel</dt> <dd>{{ hostel }}</dd> <dt>Department</dt> \
+                <dd>{{ dept }}</dd> \
+            </dl> \
+        "; 
+        $.get(details_url, function(data){
+            generated_html = Mustache.to_html(details_template, data);
+            $('.user_details').show();
+            $('.user_details').html(generated_html);
+        });
+    });
+
+    $('#add_candidate_form').submit(function(){
+        $.ajax({
+            url: $SCRIPT_ROOT+'/candidate/new',
+            type: "POST",
+            data: $(this).serialize(),
+            success: function(data){
+                $('.add_status').text(data.status_msg);
+                $('#reset_btn').click();
+            }
+        });
+        $('.user_details').hide();
         return false;
     });
 });
