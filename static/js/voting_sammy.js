26ac72543839c66189671fa807708b1783f4d95e
Prakhar Srivastav <prakhar1989@gmail.com>
2012-07-28 09:08:19 +0530

    Added sessions and persistence

--- static/js/voting_sammy.js
+++ static/js/voting_sammy.js
@@ -1,6 +1,9 @@
 (function($) {
 
-var votes_array = {};
+//This is made global only
+//for debugging purposes. Remember
+//to make this local to the context
+//votes_array = {};
 
 var fill_with_default = function(votes_array, default_string){
     //instantiates votes_array to a default value
@@ -9,16 +12,19 @@ var fill_with_default = function(votes_array, default_string){
     }
 }
 
-fill_with_default(votes_array, "default");
+//fill_with_default(votes_array, "default");
 
-var add_to_votes_array = function(votes_array, username_list, post_id){
-    //something wrong here
-    votes_array[post_id] = username_list;
-};
 
 
 var app = $.sammy('#main', function() {
     this.use('Template');
+    this.use('Session');
+
+    var votes_array = this.session('votes_array', function(){
+        return {};
+    });
+
+    if (votes_array == {}) { fill_with_default(votes_array, "default"); }
 
     this.get('#/', function(context){
         this.redirect('#/1');
@@ -26,10 +32,8 @@ var app = $.sammy('#main', function() {
 
     this.get('#/:post_id', function(context){
         this.trigger('addVotesInArray');
-        //console.log("Votes Array " + votes_array);
-        //var posts_votes = votes_array[this.params['post_id'] - 1];
-        console.log(votes_array);
         context.app.swap('');
+
         this.load('/post/' +  this.params['post_id'])
             .then(function(candidates){
                 //Use post data generated to add help_text for each post generated
@@ -37,9 +41,22 @@ var app = $.sammy('#main', function() {
                 $('.post_heading').data("post_id", candidates[0][0]["post_id"]);
                 $.each(candidates[1], function(i, candidate){
                     context.render("static/templates/candidate.template", {candidate:candidate})
-                    .appendTo(context.$element());
+                    .appendTo(context.$element())
+                    .then(function(){
+                        //Important
+                        var chosen_votes = votes_array[context.params['post_id']];
+                        console.log(chosen_votes);
+                        if (chosen_votes.length > 0 && chosen_votes != "default") {
+                            for (i=0; i < chosen_votes.length; i++) {
+                                var u_name = chosen_votes[i];
+                                $('#main').find("[data-username='" + u_name + "']")
+                                          .addClass('candidate_selected')
+                                          .find('.checkbox').addClass('checkbox_selected');
+                            }
+                        }
+                    });
                 });
-        });
+            });
     });
     
     this.bind('addVotesInArray', function(){
@@ -49,7 +66,12 @@ var app = $.sammy('#main', function() {
        for (i = 0; i < selected_candidates.length; i++) {
             selected_candidate_list.push($(selected_candidates[i]).data("username"));
        }
-       add_to_votes_array(votes_array, selected_candidate_list, current_post_id);
+       if (selected_candidate_list.length > 0) {
+           //dont add blank lists to votes_array
+           //add_to_votes_array(votes_array, selected_candidate_list, current_post_id);
+           votes_array[current_post_id] = selected_candidate_list;
+           this.session('votes_array', votes_array);
+       }
     });
 
     $(function() {
